06db71f49cad9121d6b7138071f7e4ad615430dd
Ciro Mauricio de Cristo <98125618+cirodecristo@users.noreply.github.com>
2023-01-30 19:19:20 -0300

    Tratativas de erro
    Tratativas de campos vazios

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -6,8 +6,13 @@ namespace ControleDeContatos.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage ="Digite o nome do contato")]
         public string  Nome { get; set; }
+        [Required(ErrorMessage = "Digite o email do contato")]
+        [EmailAddress(ErrorMessage ="O email informado não é valido")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Digite o celular do contato")]
+        [Phone(ErrorMessage ="O celular informado não é válido")]
         public string Celular { get; set; }
     }
 }
